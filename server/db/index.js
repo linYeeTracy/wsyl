59ce71ad4a1bac5c1aac0d8900a040e8c4a9e4bd
linYeeTracy <494787963@qq.com>
2018-11-12 15:24:10 +0800

    update

--- server/db/index.js
+++ server/db/index.js
@@ -3,10 +3,4 @@
  */
 
 exports.Custom = require('./custom');
-// exports.Role = require('./role');
-// exports.Group = require('./group');
-// exports.Scene = require('./scene');
-// exports.Project = require('./project');
-// exports.Style = require('./style');
-// exports.Material = require('./material');
-// exports.Keyword = require('./keyword');
+
